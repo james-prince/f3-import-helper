5f938a43cab1fda2ac90e31465161bbb16a4fdb6
james-prince <83412999+james-prince@users.noreply.github.com>
2024-06-13 14:41:51 +0100

    Added http-server functionality

---
+++ http-server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
+
+type endPoint struct {
+	LastImport    time.Time `json:"lastImport"`
+	NextImport    time.Time `json:"nextImport"`
+	ImportRunning bool      `json:"importRunning"`
+}
+
+var statusEndPoint endPoint
+
+func serveStatusEndPoint(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	// io.WriteString(w, "fsdfsdf")
+
+	if err := json.NewEncoder(w).Encode(statusEndPoint); err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func startHttpServer(waitGroup *sync.WaitGroup) {
+	http.HandleFunc("/status", serveStatusEndPoint)
+
+	listenPort := ":3333"
+	fmt.Printf("json endpoint available at http://localhost%s/status\n", listenPort)
+	waitGroup.Done()
+
+	err := http.ListenAndServe(listenPort, nil)
+	switch {
+	case errors.Is(err, http.ErrServerClosed):
+		os.Exit(1)
+	case err != nil:
+		os.Exit(1)
+	}
+}
